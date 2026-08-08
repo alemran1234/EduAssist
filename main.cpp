b239d54b70ec7869a5968385ebe5f0ddee83af5f
Al Emran <alemran1701@gmail.com>
2026-08-08 13:32:00 +0600

    complete Expens Module

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "include/Scholarship.h"
 #include "include/StudyPlanner.h"
+#include "include/ExpensesTracker.h"
 
 using namespace std;
 
@@ -7,28 +8,38 @@ int main() {
 
     Scholarship s;
     StudyPlanner p;
+    ExpenseTracker e;
 
     int choice;
 
     do {
-        cout << "\n========== EduAssist ==========\n";
+
+        cout << "\n===== EduAssist =====\n";
         cout << "1. Scholarship\n";
         cout << "2. Study Planner\n";
+        cout << "3. Expense Tracker\n";
         cout << "0. Exit\n";
+
         cout << "Enter your choice: ";
         cin >> choice;
 
         switch (choice) {
 
+            // =========================
+            // Scholarship
+            // =========================
             case 1: {
+
                 int scholarshipChoice;
 
                 do {
-                    cout << "\n===== Scholarship Menu =====\n";
+
+                    cout << "\n===== Scholarship =====\n";
                     cout << "1. Add Scholarship\n";
                     cout << "2. View Scholarship\n";
                     cout << "3. Search Scholarship\n";
                     cout << "0. Back\n";
+
                     cout << "Enter your choice: ";
                     cin >> scholarshipChoice;
 
@@ -47,11 +58,11 @@ int main() {
                             break;
 
                         case 0:
-                            cout << "Returning to Main Menu...\n";
+                            cout << "\nGoing back...\n";
                             break;
 
                         default:
-                            cout << "Invalid Choice!\n";
+                            cout << "\nInvalid choice!\n";
                     }
 
                 } while (scholarshipChoice != 0);
@@ -59,15 +70,22 @@ int main() {
                 break;
             }
 
+
+            // =========================
+            // Study Planner
+            // =========================
             case 2: {
+
                 int plannerChoice;
 
                 do {
+
                     cout << "\n===== Study Planner =====\n";
                     cout << "1. Add Task\n";
                     cout << "2. View Task\n";
                     cout << "3. Complete Task\n";
                     cout << "0. Back\n";
+
                     cout << "Enter your choice: ";
                     cin >> plannerChoice;
 
@@ -86,11 +104,11 @@ int main() {
                             break;
 
                         case 0:
-                            cout << "Returning to Main Menu...\n";
+                            cout << "\nGoing back...\n";
                             break;
 
                         default:
-                            cout << "Invalid Choice!\n";
+                            cout << "\nInvalid choice!\n";
                     }
 
                 } while (plannerChoice != 0);
@@ -98,12 +116,62 @@ int main() {
                 break;
             }
 
+
+            // =========================
+            // Expense Tracker
+            // =========================
+            case 3: {
+
+                int expenseChoice;
+
+                do {
+
+                    cout << "\n===== Expense Tracker =====\n";
+                    cout << "1. Add Expense\n";
+                    cout << "2. View Expense\n";
+                    cout << "3. Search Expense\n";
+                    cout << "0. Back\n";
+
+                    cout << "Enter your choice: ";
+                    cin >> expenseChoice;
+
+                    switch (expenseChoice) {
+
+                        case 1:
+                            e.addExpense();
+                            break;
+
+                        case 2:
+                            e.viewExpense();
+                            break;
+
+                        case 3:
+                            e.searchExpense();
+                            break;
+
+                        case 0:
+                            cout << "\nGoing back...\n";
+                            break;
+
+                        default:
+                            cout << "\nInvalid choice!\n";
+                    }
+
+                } while (expenseChoice != 0);
+
+                break;
+            }
+
+
+            // =========================
+            // Exit
+            // =========================
             case 0:
-                cout << "\nThank you for using EduAssist!\n";
+                cout << "\nExiting EduAssist...\n";
                 break;
 
             default:
-                cout << "\nInvalid Choice!\n";
+                cout << "\nInvalid choice!\n";
         }
 
     } while (choice != 0);
