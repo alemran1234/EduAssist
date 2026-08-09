9078663b2fae2f2d20bf6b0b33b00829ce21097a
Sijan Kumar Saha <siju55300@gmail.com>
2026-08-09 20:26:42 +0600

    Create Report.h

---
+++ include/Report.h
@@ -0,0 +1,14 @@
+#ifndef REPORT_H
+#define REPORT_H
+
+#include <iostream>
+#include <string>
+
+class Report {
+public:
+    Report();
+    void generateSummaryReport();
+    void displayReport() const;
+};
+
+#endif // REPORT_H
