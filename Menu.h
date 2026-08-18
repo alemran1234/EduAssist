05453dd6cfc3101ac298d5b05086d6673e8475ee
LAMIA <lamiaaman3@gmail.com>
2026-08-19 01:15:54 +0600

    Create Menu.h

---
+++ Menu.h
@@ -0,0 +1,15 @@
+#ifndef EDUASSIST_MENU_H
+#define EDUASSIST_MENU_H
+
+class Menu
+{
+public:
+    void displayMainMenu();
+    void displayStudentMenu();
+    void displayScholarshipMenu();
+    void displayStudyPlannerMenu();
+    void displayExpenseMenu();
+    void displayResourceMenu();
+};
+
+#endif // EDUASSIST_MENU_H
